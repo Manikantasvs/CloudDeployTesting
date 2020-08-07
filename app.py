9b9d0eeb2840afc7e856d4f55965df4003a67445
Sannidhi Venkata Subba Manikanta Swami <44739674+Manikantasvs@users.noreply.github.com>
2020-08-07 13:50:52 +0530

    Add files via upload
    ML Cloud Deployment Testing

---
+++ app.py
@@ -0,0 +1,107 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+# Keras
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+MODEL_PATH = 'models/Object_Recognition_h5_model.h5'
+
+# Load your trained model
+model = load_model(MODEL_PATH)
+#model._make_predict_function()          # Necessary
+print('Model loaded. Start serving...')
+
+# You can also use pretrained model from Keras
+# Check https://keras.io/applications/
+#from keras.applications.resnet50 import ResNet50
+#model = ResNet50(weights='imagenet')
+#model.save('')
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+def model_predict(img_path, model):
+    print("Entered model_predict---1")
+    img = image.load_img(img_path, target_size=(32, 32))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+    x = preprocess_input(x, mode='caffe')
+
+    preds = model.predict(x)
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+    print("Entered index---2")
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    print("Entered upload---3")
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+        names = ['airplane',
+        'automobile',
+        'bird',
+        'cat',
+        'deer',
+        'dog',
+        'frog',
+        'horse',
+        'ship',
+        'truck']
+        # Process your result for human
+        #pred_class = preds.argmax(axis=-1)            # Simple argmax
+        #print("\nPrinting Predictions-------------------\n",preds,"\n-------------------------------\n")
+        ####pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+        max_val = max(preds[0])
+        print("\nMaximum Value = ",max_val)
+        ind = np.where(preds[0] == max_val)
+        ind = int(ind[0])
+        print("**************** Index = ",ind)
+        print("\nMax_Index = ",ind)
+        print("\nPrinting Predictions-------------------\n",preds,"\n-------------------------------\n")
+        res = names[ind]
+        result = str(res)               # Convert to string
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    print("Entered Main----0")
+    app.run(debug=True)
+
